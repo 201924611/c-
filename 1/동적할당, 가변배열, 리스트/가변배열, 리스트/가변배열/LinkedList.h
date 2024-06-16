196099b072f217506c7540c58d7a8a95bdd729b4
201924611 <104231992+201924611@users.noreply.github.com>
2024-06-16 16:18:03 +0900

    message

--- 1/동적할당, 가변배열, 리스트/가변배열, 리스트/가변배열/LinkedList.h
+++ 1/동적할당, 가변배열, 리스트/가변배열, 리스트/가변배열/LinkedList.h
@@ -1,24 +1,62 @@
 #pragma once
 
-
-typedef struct _tagNode
+template<typename T>
+struct tNode
 {
-	int		data;
-	struct _tagNode* Next;
-}tNode;
+	T					data;
+	struct tNode*		Next;
+	
+};
 
-typedef struct _tagList
+
+struct tLinkedList
 {
-	tNode	*pHeadNode;
-	int		iCount;
-}tLinkedList;
+	struct tNode*		pHeadNode;
+	int					iCount;
+};
 
 // 리스트 초기화
-void InitList(tLinkedList* _pList);
+void InitList(tLinkedList* _pList)
+{
+	_pList->pHeadNode = nullptr;
+	_pList->iCount = 0;
+}
 
 // 연결형 리스트 데이터 추가
-void PushBack(tLinkedList* _pList, int _iData);
-void PushFront(tLinkedList* _pList, int _iData);
+template<typename T>
+void PushBack(tLinkedList* _pList, T _iData)
+{
+	tNode<T>* pNode = (tNode<T>*)malloc(sizeof(tNode<T>));
+
+
+	pNode->data = _iData;
+	pNode->Next = nullptr;
+
+	tNode<T>* node;
+
+	// 추가한 데이터가 처음인지 아닌지
+	if (0 == _pList->iCount)
+	{
+		_pList->pHeadNode = pNode;
+	}
+	else
+	{
+		// 현재 가장 마지막 노드를 찾아서,
+		// 현재
+		node = _pList->pHeadNode;
+		while (node->Next)
+		{
+			node = node->Next;
+		}
+		node->Next = pNode;
+	}
+
+	++_pList->iCount;
+}
+
+
+template<typename T>
+void PushFront(tLinkedList* _pList, T _iData);
 
 // 연결형 리스트 메모리 해제
 void ReleaseList(tLinkedList* _pList);
